64eff3966eb1232c68ffc44cd2a956a53fd2e9a2
Guilherme Fabrin Franco <guilherme.fabrin@gmail.com>
2019-04-05 14:49:20 -0300

    Update Helper.src to get bootstrap version.

--- src/Helper.php
+++ src/Helper.php
@@ -13,9 +13,9 @@ class Helper
             if ($required === 'false') $required = false;
             else if ($required !== 'false') $required = Config::val('default_required', 'false');
         }
-        $bootstrap_version = Config::val('boostrap_version', '4.3.1');
+        $json = json_decode(file_get_contents(dirname(__FILE__)."/../../../../composer.json"), JSON_OBJECT_AS_ARRAY);
         ob_start();
-        include 'partials/'.$bootstrap_version."/".$partial.".php";
+        include 'partials/'.$json['require']['twbs/bootstrap']."/".$partial.".php";
         return ob_get_clean();
     }
 
